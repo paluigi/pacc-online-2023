8ebe9f809f4b6ae971bad9253064226116b79d94
Luigi <paluigi@gmail.com>
2023-09-21 19:28:23 +0200

    new file

--- 105/weatherflow_original.py
+++ 105/weatherflow_original.py
@@ -1,11 +1,14 @@
 import httpx
 from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
+
+
 @task
 def mark_it_down(temp):
     markdown_report = f"""# Weather Report
     
 ## Recent weather
+
 | Time        | Revenue |
 |:--------------|-------:|
 | Now | {temp} |
@@ -16,10 +19,8 @@ def mark_it_down(temp):
         markdown=markdown_report,
         description="Very scientific weather report",
     )
-@flow
-def fetch_cat_fact():
-    return httpx.get('https://catfact.ninja/fact')
-    
+
+
 @flow
 def fetch_weather(lat: float, lon: float):
     base_url = "https://api.open-meteo.com/v1/forecast/"
@@ -29,12 +30,7 @@ def fetch_weather(lat: float, lon: float):
     )
     most_recent_temp = float(weather.json()["hourly"]["temperature_2m"][0])
     mark_it_down(most_recent_temp)
-    return most_recent_temp
-@flow
-def cat_weather(lat: float, lon: float):
-    cat_fact = fetch_cat_fact()
-    weather_fact = fetch_weather(lat, lon)
-    print(f'Are cats {cat_fact }going to get wet {weather_fact}?')
+
+
 if __name__ == "__main__":
     fetch_weather(38.9, -77.0)
-
